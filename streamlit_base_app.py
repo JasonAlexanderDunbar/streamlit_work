945c57c9e233fea22bd03822505a93f5a8a69671
JasonAlexanderDunbar <81414117+JasonAlexanderDunbar@users.noreply.github.com>
2021-06-22 15:13:00 +0200

    Update streamlit_base_app.py

--- streamlit_base_app.py
+++ streamlit_base_app.py
@@ -53,7 +53,7 @@ def main():
 	if selection == "Information":
 		st.info("General Information")
 		# You can read a markdown file from supporting resources folder
-		st.markdown("Many companies are built around lessening one’s environmental impact or carbon footprint. They offer products and services that are environmentally friendly and sustainable, in line with their values and ideals. They would like to determine how people perceive climate change and whether or not they believe it is a real threat. This would add to their market research efforts in gauging how their product/service may be received.With this context, EDSA is challenging you during the Classification Sprint with the task of creating a Machine Learning model that is able to classify whether or not a person believes in climate change, based on their novel tweet data.Providing an accurate and robust solution to this task gives companies access to a broad base of consumer sentiment, spanning multiple demographic and geographic categories - thus increasing their insights and informing future marketing strategies") # Will add an article about climate change (Jason)
+		st.markdown("Many coou during the Classification Sprle demographic and geographic categ)
 
 		st.subheader("Raw Twitter data and label")
 		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
@@ -78,10 +78,7 @@ def main():
 			# more human interpretable.
 			st.success("Text Categorized as: {}".format(prediction))
 
-                elif st.button("Classify with Intermediate Model"):
-         		vect_tect = tweet_cv.transform([tweet_text]).toarray()
-                    	predictor = joblib.load(open(os.path.join("NaiveBayesClassification.pkl"), "rb"))
-                    	prediction = predictor.predict(vect_tweet)
+                
 
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
